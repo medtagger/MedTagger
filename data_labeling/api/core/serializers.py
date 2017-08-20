687e4375011cc33cfce3128d0996c7bd3299d3b2
Jakub Powierza <jpowie01@gmail.com>
2017-08-20 14:08:15 +0000

    Add simple Flask-RESTPlus API with status endpoint

---
+++ data_labeling/api/core/serializers.py
@@ -0,0 +1,9 @@
+"""Module responsible for storage of serializers used in Core endpoint"""
+from flask_restplus import fields
+
+from data_labeling.api import api
+
+
+status = api.model('Status model', {
+    'success': fields.Boolean(description='Should be True if everything is all right.'),
+})
--- tests/test_hello_world.py
+++
@@ -1,6 +0,0 @@
-from data_labeling.hello_world import say_hello
-
-
-def test_say_hello():
-    """Test if function say hello and always return 4"""
-    assert say_hello() == 4
