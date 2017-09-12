779f66fa81bc397b85e62604879f038d60b52772
Darkoon <darkon.darkon@gmail.com>
2017-09-12 22:03:06 +0200

    Marker Page, basic page navigation

--- src/app/marker-page/marker-page.component.ts
+++ src/app/marker-page/marker-page.component.ts
@@ -2,11 +2,14 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-marker-page',
-  templateUrl: './marker-page.component.html'
+  templateUrl: './marker-page.component.html',
 })
 export class MarkerPageComponent implements OnInit {
+  public currentImageNr: number;
+  private imageBatch: string[];
 
   constructor() {
+    this.currentImageNr = 0;
   }
 
   ngOnInit() {
