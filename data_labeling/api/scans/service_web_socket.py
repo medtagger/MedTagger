010c18472614759585bbab0c24791e7ac899a94f
Jakub Powierza <jpowie01@gmail.com>
2017-10-22 00:58:36 +0200

    Emit 'ack' after new Slice upload

--- data_labeling/api/scans/service_web_socket.py
+++ data_labeling/api/scans/service_web_socket.py
@@ -47,6 +47,7 @@ class Slices(Namespace):
         image_bytes = io.BytesIO(image)
         dicom_image_file = dicom.read_file(image_bytes, force=True)
         add_new_slice(scan_id, dicom_image_file)
+        emit('ack')
 
 
 # Register above namespace
