3051a22f30d5ea3f46f86ba7eea4e061867cd1d2
k_olszewska <k.olszewskaaa@gmail.com>
2017-10-25 17:05:46 +0200

    Deleted utils folder

--- scripts/clear_hbase.py
+++ scripts/clear_hbase.py
@@ -1,9 +1,18 @@
 """Script that can clear existing HBase tables"""
 from starbase import Connection
-
 from data_labeling import HBASE_SCHEMA
 from data_labeling.config import ConfigurationFile
-from scripts.utils.ask_user_question import ask_user_question
+
+
+def ask_user_question(prompt_message: str) -> bool:
+    """Ask user a question and ask him/her for True/False answer (default answer is False)
+
+    :param prompt_message: message that will be prompted to user
+    :return: boolean information if user agrees or not
+    """
+    answer = input(prompt_message + ' [y/N] ')
+    return answer.lower() in ['y', 'yes', 't', 'true']
+
 
 configuration = ConfigurationFile()
 host = configuration.get('hbase', 'host', fallback='localhost')
--- scripts/utils/ask_user_question.py
+++
@@ -1,8 +0,0 @@
-def ask_user_question(prompt_message: str) -> bool:
-    """Ask user a question and ask him/her for True/False answer (default answer is False)
-
-    :param prompt_message: message that will be prompted to user
-    :return: boolean information if user agrees or not
-    """
-    answer = input(prompt_message + ' [y/N] ')
-    return answer.lower() in ['y', 'yes', 't', 'true']
