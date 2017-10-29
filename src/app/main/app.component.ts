bd485552af563334d2f4a3199221dff7adad7a9c
Jakub Powierza <jpowie01@gmail.com>
2017-10-29 04:18:02 +0100

    Page title on top

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private router: Router) {};
+  pageTitle = '';
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {};
+
+  ngOnInit() {
+    this.router.events
+      .filter((event) => event instanceof NavigationEnd)
+      .map(() => this.activatedRoute)
+      .map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      })
+      .filter((route) => route.outlet === 'primary')
+      .mergeMap((route) => route.data)
+      .subscribe((event) => this.pageTitle = event['title']);
+  }
 
   get isLoginPage() : boolean {
     return this.router.url === '/login';
