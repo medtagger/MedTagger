9dc9d0fbd39920f5df06dbd40c6832d61ffe6f75
Jakub Powierza <jpowie01@gmail.com>
2017-10-29 17:53:24 +0000

    Complex Labels

--- data_labeling/api/scans/service_rest.py
+++ data_labeling/api/scans/service_rest.py
@@ -3,10 +3,10 @@ from typing import Any
 from flask import request
 from flask_restplus import Resource
 
-from data_labeling.types import ScanID, CuboidLabelPosition, CuboidLabelShape
+from data_labeling.types import ScanID
 from data_labeling.api import api
 from data_labeling.api.scans import serializers
-from data_labeling.api.scans.business import create_empty_scan, get_random_scan, add_cuboid_label
+from data_labeling.api.scans.business import create_empty_scan, get_random_scan, add_label
 
 scans_ns = api.namespace('scans', 'Methods related with scans')
 
@@ -38,21 +38,19 @@ class Random(Resource):
         return get_random_scan()
 
 
-@scans_ns.route('/<string:scan_id>/label/cuboid')
+@scans_ns.route('/<string:scan_id>/label')
 @scans_ns.param('scan_id', 'Scan identifier')
-class LabelCuboid(Resource):
-    """Endpoint that saves cuboid label for given scan"""
+class Label(Resource):
+    """Endpoint that stores label for given scan"""
 
     @staticmethod
-    @scans_ns.expect(serializers.cuboid_label, validate=True)
-    @scans_ns.doc(description='Saves cuboid label and assigns it to given scan.')
+    @scans_ns.expect(serializers.label, validate=True)
+    @scans_ns.doc(description='Stores label and assigns it to given scan.')
     @scans_ns.doc(responses={201: 'Successfully saved', 400: 'Invalid arguments', 404: 'Could not find scan'})
     def post(scan_id: ScanID) -> Any:
-        """Method responsible for saving new cuboid label for given scan"""
+        """Method responsible for saving new label for given scan"""
         payload = request.json
-
-        position = CuboidLabelPosition(x=payload['x'], y=payload['y'], z=payload['z'])
-        shape = CuboidLabelShape(width=payload['width'], height=payload['height'], depth=payload['depth'])
-        label_id = add_cuboid_label(scan_id, position, shape)
+        selections = payload.get('selections', [])
+        label_id = add_label(scan_id, selections)
 
         return {'label_id': label_id}, 201
