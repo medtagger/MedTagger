038534cd3be95ae52a59d518d18916c32225dd94
Jakub Powierza <jpowie01@gmail.com>
2017-12-15 19:18:39 +0100

    Travis CI

--- scripts/is_change_in_subproject.py
+++ scripts/is_change_in_subproject.py
@@ -4,25 +4,32 @@ import os
 import subprocess
 import argparse
 
-parser = argparse.ArgumentParser(description='Checks if directory contains any change.')
-parser.add_argument('directory', type=str, help='Directory in which should be changes.')
-args = parser.parse_args()
-print(args.directory)
+
+def get_root_dir(path):
+    return os.path.dirname(path).split('/')[0]
+
+def run():
+    exit(0)
+
+def do_not_run():
+    exit(1)
+
 
 logging.basicConfig(level=logging.INFO)
+parser = argparse.ArgumentParser(description='Checks if directory contains any change.')
+parser.add_argument('subproject_name', type=str, help='Directory in which should be changes.')
+args = parser.parse_args()
+
 if os.environ.get('TRAVIS_PULL_REQUEST', False):
-    commit_range = 'master..' + os.environ.get('TRAVIS_COMMIT', '')
-    logging.info('Finding all containers that changed in %s', commit_range)
+    logging.info('This is a Pull Request, so we will run CI only for changed subprojects.')
     diff_lines = subprocess.check_output(['git', 'diff', '--name-only', 'origin/master']).split()
-    print(diff_lines)
-    changed_folders = {os.path.dirname(line) for line in diff_lines if os.path.dirname(line)}
-    print(changed_folders)
-    logging.info('The following folders contain changes: %s', changed_folders)
-    if args.directory in changed_folders:
-        exit(0)
+    changed_subprojects = {get_root_dir(line) for line in diff_lines if os.path.dirname(line)}
+    logging.info('The following subprojects contain changes: %s', changed_subprojects)
+    if args.subproject_name in changed_subprojects:
+        run()
     else:
-        exit(1)
+        do_not_run()
 else:
     logging.info('Running all tests as it is not a Pull Request.')
-    exit(0)
+    run()
 
