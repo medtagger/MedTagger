4fbcf499780d3ad33fb20da0789b68554a1f4d85
Jakub Powierza <jpowie01@gmail.com>
2017-12-15 20:44:14 +0100

    Travis CI

--- scripts/test_change_in_subproject.py
+++ scripts/test_change_in_subproject.py
@@ -5,22 +5,24 @@ import subprocess
 import argparse
 
 
-def get_root_dir(path):
-    return os.path.dirname(path).split('/')[0]
-
-def run():
-    logging.info('Let\'s run the CI!')
-    exit(0)
-
-def do_not_run():
-    logging.info('This subproject does not contain changes.')
-    exit(1)
-
-
 logging.basicConfig(level=logging.INFO)
 parser = argparse.ArgumentParser(description='Checks if directory contains any change.')
 parser.add_argument('subproject_name', type=str, help='Directory in which should be changes.')
+parser.add_argument('command', type=str, help='Command to be executed if there was a change.')
 args = parser.parse_args()
+
+
+def get_root_dir(path):
+    return os.path.dirname(path).split('/')[0]
+
+def run(command):
+    logging.info('Let\'s run the CI!')
+    subprocess.run(command, shell=True)
+
+def do_not_run():
+    logging.info('This subproject does not contain changes.')
+    exit(0)
+
 
 if os.environ.get('TRAVIS_PULL_REQUEST', False):
     logging.info('This is a Pull Request, so we will run CI only for changed subprojects.')
@@ -28,10 +30,10 @@ if os.environ.get('TRAVIS_PULL_REQUEST', False):
     changed_subprojects = {get_root_dir(line) for line in diff_lines if os.path.dirname(line)}
     logging.info('The following subprojects contain changes: %s', changed_subprojects)
     if args.subproject_name in changed_subprojects:
-        run()
+        run(args.command)
     else:
         do_not_run()
 else:
     logging.info('Running all tests as it is not a Pull Request.')
-    run()
+    run(args.command)
 
