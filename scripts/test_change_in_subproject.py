dcdd8c5183b12a73300be33988a6022246b4060c
Jakub Powierza <jpowie01@gmail.com>
2017-12-16 22:33:31 +0100

    Change the way Travis triggers testing (#3)

--- scripts/test_change_in_subproject.py
+++ scripts/test_change_in_subproject.py
@@ -13,6 +13,8 @@ parser.add_argument('subproject_name', type=str, help='Directory in which should
 parser.add_argument('--command', dest='command', type=str, help='Command to be executed if there was a change.')
 args = parser.parse_args()
 
+BRANCHES_WITH_FULL_TESTS = ['master']
+
 
 def get_root_dir(path):
     return os.path.dirname(path).split('/')[0]
@@ -31,9 +33,10 @@ def do_not_run():
     exit(0)
 
 
-logging.info('This is a %s event.', os.environ.get('TRAVIS_EVENT_TYPE'))
-if os.environ.get('TRAVIS_EVENT_TYPE') == 'pull_request':
-    logging.info('This is a Pull Request event, so we will run CI only for changed subprojects.')
+logging.info('This is a CI on branch %s.', os.environ.get('TRAVIS_BRANCH'))
+logging.info('Full testing on all subprojects run on these branches: %s.', BRANCHES_WITH_FULL_TESTS)
+if os.environ.get('TRAVIS_BRANCH') not in BRANCHES_WITH_FULL_TESTS:
+    logging.info('This is not a fully testing branch, so we will run tests only for changed subprojects.')
     diff_lines = subprocess.check_output(['git', 'diff', '--name-only', 'origin/master']).split()
     changed_subprojects = {get_root_dir(line) for line in diff_lines if os.path.dirname(line)}
     logging.info('The following subprojects contain changes: %s', changed_subprojects)
@@ -42,6 +45,6 @@ if os.environ.get('TRAVIS_EVENT_TYPE') == 'pull_request':
     else:
         do_not_run()
 else:
-    logging.info('Running all tests as it is not a Pull Request.')
+    logging.info('Running all tests on this branch.')
     run(args.command)
 
