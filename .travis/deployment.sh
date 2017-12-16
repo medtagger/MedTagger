9f5c891b767322c2b01838da5ef06ddf9a52ecd7
Jakub Powierza <jpowie01@gmail.com>
2017-12-17 00:52:29 +0100

    Move deployments to the separate stage

--- .travis/deployment.sh
+++ .travis/deployment.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$1" == "staging" ]; then
+    echo "Enabling SSH Agent..."
     eval "$(ssh-agent -s)"
-    ssh-add ./.travis/staging.key
+    echo "Adding private key to the staging server..."
+    ssh-add /tmp/.travis/staging.key
+    echo "Executing deployment on the server..."
     ssh $SSH_DESTINATION_HOST__STAGING -p $SSH_DESTINATION_PORT__STAGING "echo Hello"
 fi
