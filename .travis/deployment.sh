902080a54e63f3ab272d576d36d33b8335b6c194
Jakub Powierza <jpowie01@gmail.com>
2017-12-17 01:08:09 +0100

    Move decrypting private key to the deployment stage and create directory before doing it

--- .travis/deployment.sh
+++ .travis/deployment.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$1" == "staging" ]; then
+    echo "Create directory for staging key..."
+    mkdir -p /tmp/.travis/
+    echo "Decrypt private key to the staging server..."
+    openssl aes-256-cbc -K $encrypted_9953cb5dbc4f_key -iv $encrypted_9953cb5dbc4f_iv -in .travis/staging.key.enc -out /tmp/.travis/staging.key -d
     echo "Enabling SSH Agent..."
     eval "$(ssh-agent -s)"
     echo "Adding private key to the staging server..."
