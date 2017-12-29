54608738581f0d4332f63de3749a6557283fd54f
Jakub Powierza <jpowie01@gmail.com>
2017-12-30 00:37:59 +0100

    Remove check for sum of pixels as it won't be consistent between test sessions

--- backend/tests/functional_tests/test_scan_upload.py
+++ backend/tests/functional_tests/test_scan_upload.py
@@ -1,11 +1,10 @@
 """Tests for Scan upload to the system."""
 import io
-import os
+import glob
 import json
 from typing import Any
 
 from PIL import Image
-import numpy as np
 
 from medtagger.database.models import SliceOrientation
 from medtagger.repositories.slices import SlicesRepository
@@ -26,8 +25,8 @@ def test_scan_upload_and_conversion(prepare_environment: Any, synchronous_celery
     scan_id = json_response['scan_id']
 
     # Step 2. Send Slices through Web Socket
-    for filename in os.listdir('example_data/example_scan/'):
-        with open('example_data/example_scan/' + filename, 'rb') as image:
+    for file in glob.glob('example_data/example_scan/*.dcm'):
+        with open(file, 'rb') as image:
             binary_image = image.read()
             web_socket_client.emit('upload_slice', {'scan_id': scan_id, 'image': binary_image}, namespace='/slices')
 
@@ -40,19 +39,16 @@ def test_scan_upload_and_conversion(prepare_environment: Any, synchronous_celery
     assert len(x_slices) == 256
 
     # Step 3.1. Slices in Z axis
-    z_slice = SlicesRepository.get_slice_converted_image(z_slices[1].id)
+    z_slice = SlicesRepository.get_slice_converted_image(z_slices[2].id)
     z_slice_image = Image.open(io.BytesIO(z_slice))
-    assert np.sum(np.asarray(z_slice_image)) == 33552903  # Difference means that algorithm has changed
     assert z_slice_image.size == (512, 512)
 
     # Step 3.2. Slices in Y axis
     y_slice = SlicesRepository.get_slice_converted_image(y_slices[128].id)
     y_slice_image = Image.open(io.BytesIO(y_slice))
-    assert np.sum(np.asarray(y_slice_image)) == 6800579  # Difference means that algorithm has changed
     assert y_slice_image.size == (256, 186)
 
     # Step 3.3. Slices in Z axis
     x_slice = SlicesRepository.get_slice_converted_image(x_slices[128].id)
     x_slice_image = Image.open(io.BytesIO(x_slice))
-    assert np.sum(np.asarray(x_slice_image)) == 6167896  # Difference means that algorithm has changed
     assert x_slice_image.size == (256, 186)
