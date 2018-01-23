a801d68aa94b5f7504f8b64ab965e78ececeee8e
Jakub Powierza <jpowie01@gmail.com>
2018-01-23 16:06:20 +0100

    PROD builds should point to API on the same port as UI (#48)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: true,
-  API_URL: window.location.protocol + '//' + window.location.hostname + '/api/v1',
-  WEBSOCKET_URL: window.location.protocol + '//' + window.location.hostname,
+  API_URL: window.location.origin + '/api/v1',
+  WEBSOCKET_URL: window.location.origin,
 };
