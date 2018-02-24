b091c72cff7473addd2895dcfbba9ecd1a6b695e
Dawid Paluchowski <darkon.darkon@gmail.com>
2018-02-24 22:13:27 +0100

    Removed doubled variable init line

---
+++ frontend/src/app/components/movie-viewer/movie-viewer.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ScanViewerComponent } from './scan-viewer.component';
+
+describe('ScanViewerComponent', () => {
+  let component: ScanViewerComponent;
+  let fixture: ComponentFixture<ScanViewerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ScanViewerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ScanViewerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
