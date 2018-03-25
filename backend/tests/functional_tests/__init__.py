472d7bccc572a8b2cd3a2a7a908f0ef7b5958f71
Jakub Powierza <jpowie01@gmail.com>
2018-03-25 22:07:33 +0200

    Fix functional tests

--- backend/tests/functional_tests/__init__.py
+++ backend/tests/functional_tests/__init__.py
@@ -1,17 +1,16 @@
 """Module for global methods that may be useful during application testing."""
 from typing import Any
 
-from medtagger.api.rest import app
-from medtagger.api.websocket import web_socket
-
 
 def get_api_client() -> Any:
     """Get API client for testing purpose."""
+    from medtagger.api.rest import app
     app.testing = True
     return app.test_client()
 
 
 def get_web_socket_client(namespace: str = None) -> Any:
     """Get Web Socket client for testing purpose."""
+    from medtagger.api.websocket import app, web_socket
     app.testing = True
     return web_socket.test_client(app, namespace=namespace)
