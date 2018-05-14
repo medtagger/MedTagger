81a7f446cc4903634be297579c0009a0e2cd47ff
Jakub Powierza <jpowie01@gmail.com>
2018-05-14 23:04:25 +0200

    Move DB migrations to separate Docker

--- backend/scripts/dev__prepare_backend.sh
+++ backend/scripts/dev__prepare_backend.sh
@@ -2,8 +2,8 @@
 echo "Waiting for dependencies..."
 . scripts/wait_for_dependencies.sh
 
-echo "Migrating databases..."
-alembic upgrade head
+echo "Running migrations..."
+make run_database_migrations
 
 echo "Apply database fixtures..."
 python3.6 medtagger/database/fixtures.py
