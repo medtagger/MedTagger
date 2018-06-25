45372e250ed7972231e415de9c527bf2b50b14a4
piotrwalkusz1 <piotrwalkusz1@users.noreply.github.com>
2018-06-25 19:31:41 +0200

    Add backend for Point Tool (#297)
    * Add backend for Point Tool
    
    * Fix linter error
    
    * Change wrong type

---
+++ backend/alembic/versions/4e7789e84f5d_add_point_label_element.py
@@ -0,0 +1,61 @@
+"""Add Point Label Element
+
+Revision ID: 4e7789e84f5d
+Revises: 9a6cd75ba23f
+Create Date: 2018-06-10 07:59:05.951360
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ENUM
+
+from medtagger.storage import create_session
+
+
+# revision identifiers, used by Alembic.
+revision = '4e7789e84f5d'
+down_revision = '9a6cd75ba23f'
+branch_labels = None
+depends_on = None
+
+
+session = create_session()
+old_label_tool_enum = ENUM('RECTANGLE', 'BRUSH',  name='label_tool', create_type=False)
+tmp_label_tool_enum = ENUM('RECTANGLE', 'BRUSH', 'POINT', name='label_tool_', create_type=False)
+new_label_tool_enum = ENUM('RECTANGLE', 'BRUSH', 'POINT', name='label_tool', create_type=False)
+
+
+def upgrade():
+    # Add Point as available Tool
+    tmp_label_tool_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE "LabelElements" ALTER COLUMN tool TYPE label_tool_ USING tool::text::label_tool_')
+    old_label_tool_enum.drop(op.get_bind(), checkfirst=False)
+    new_label_tool_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE "LabelElements" ALTER COLUMN tool TYPE label_tool USING tool::text::label_tool')
+    tmp_label_tool_enum.drop(op.get_bind(), checkfirst=False)
+
+    # Create table in PostgreSQL
+    op.create_table('PointLabelElements',
+                    sa.Column('id', sa.String(), nullable=False),
+                    sa.Column('x', sa.Float(), nullable=False),
+                    sa.Column('y', sa.Float(), nullable=False),
+                    sa.ForeignKeyConstraint(['id'], ['LabelElements.id'],
+                                            name=op.f('fk_PointLabelElement_id_LabelElements')),
+                    sa.PrimaryKeyConstraint('id', name=op.f('pk_PointLabelElement'))
+                    )
+
+
+def downgrade():
+    op.drop_table('PointLabelElements')
+
+    # Remove all elements that were labeled with Point
+    label_elements_table = sa.sql.table('LabelElements', sa.Column('tool', new_label_tool_enum, nullable=False))
+    op.execute(label_elements_table.delete().where(label_elements_table.c.tool == 'POINT'))
+
+    # Remove Brush as available Tool
+    tmp_label_tool_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE "LabelElements" ALTER COLUMN tool TYPE label_tool_ USING tool::text::label_tool_')
+    new_label_tool_enum.drop(op.get_bind(), checkfirst=False)
+    old_label_tool_enum.create(op.get_bind(), checkfirst=False)
+    op.execute('ALTER TABLE "LabelElements" ALTER COLUMN tool TYPE label_tool USING tool::text::label_tool')
+    tmp_label_tool_enum.drop(op.get_bind(), checkfirst=False)
--- backend/tests/functional_tests/test_adding_new_label.py
+++ backend/tests/functional_tests/test_adding_new_label.py
@@ -57,3 +57,50 @@ def test_add_brush_label(prepare_environment: Any, synchronous_celery: Any) -> N
     label_element_id = json_response['elements'][0]['label_element_id']
     brush_label_element = BrushLabelElement.get(id=label_element_id)
     assert brush_label_element.image
+
+
+def test_add_point_label(prepare_environment: Any, synchronous_celery: Any) -> None:
+    """Test for adding a Label made with Point tool."""
+    api_client = get_api_client()
+    user_token = get_token_for_logged_in_user('admin')
+
+    # Step 1. Add Scan to the system
+    payload = {'category': 'KIDNEYS', 'number_of_slices': 3}
+    response = api_client.post('/api/v1/scans/', data=json.dumps(payload),
+                               headers=get_headers(token=user_token, json=True))
+    assert response.status_code == 201
+    json_response = json.loads(response.data)
+    scan_id = json_response['scan_id']
+
+    # Step 2. Label it with Brush
+    create_tag_and_assign_to_category('EXAMPLE_TAG', 'Example tag', 'KIDNEYS')
+    payload = {
+        'elements': [{
+            'slice_index': 0,
+            'x': 0.25,
+            'y': 0.5,
+            'tag': 'EXAMPLE_TAG',
+            'tool': 'POINT',
+        }],
+        'labeling_time': 12.34,
+    }
+    data = {
+        'label': json.dumps(payload),
+    }
+    response = api_client.post('/api/v1/scans/{}/label'.format(scan_id), data=data,
+                               headers=get_headers(token=user_token, multipart=True))
+    assert response.status_code == 201
+    json_response = json.loads(response.data)
+    assert isinstance(json_response, dict)
+    label_id = json_response['label_id']
+    assert isinstance(label_id, str)
+    assert len(label_id) >= 1
+
+    # Step 3. Fetch details about above Label
+    response = api_client.get('/api/v1/labels/' + label_id, headers=get_headers(token=user_token))
+    assert response.status_code == 200
+    json_response = json.loads(response.data)
+    assert isinstance(json_response, dict)
+    assert len(json_response['elements']) == 1
+    assert json_response['elements'][0]['x'] == 0.25
+    assert json_response['elements'][0]['y'] == 0.5
