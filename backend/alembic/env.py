12f5431b0b9e476d2ad223d8699d024626aaa86e
piotrwalkusz1 <piotrwalkusz1@users.noreply.github.com>
2018-06-25 16:55:38 +0200

    Make alembic detect type change (#304)

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -38,7 +38,7 @@ def run_migrations_offline():
     script output.
 
     """
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True, compare_type=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -55,7 +55,8 @@ def run_migrations_online():
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata
+            target_metadata=target_metadata,
+            compare_type=True
         )
 
         with context.begin_transaction():
