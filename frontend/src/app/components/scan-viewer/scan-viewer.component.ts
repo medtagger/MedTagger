19b57caf6dbf8131df99094b0ab9cf9740826829
Jacek Szemplinski <j.szemplinski@wp.eu>
2018-08-03 22:52:14 +0200

    Fix issues with slider being out of focus in Firefox. (#354)
    * Fix issues with slider being out of focus in Firefox.
    
    * Add comment about browser-specific fix.

--- frontend/src/app/components/scan-viewer/scan-viewer.component.ts
+++ frontend/src/app/components/scan-viewer/scan-viewer.component.ts
@@ -71,7 +71,10 @@ export class ScanViewerComponent implements OnInit, AfterViewInit {
     }
 
     public sliderFocus() {
-        this.slider._elementRef.nativeElement.focus();
+        // setTimeout() fixes slider focus issues in IE/Firefox
+        window.setTimeout(() => {
+            this.slider._elementRef.nativeElement.focus();
+        }, 10);
     }
 
     public setSelectors(newSelectors: Array<Selector<SliceSelection>>) {
