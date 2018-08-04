51a16de1c31ed782946b6ed40510375b3f36b675
Jacek Szemplinski <j.szemplinski@wp.eu>
2018-08-04 17:03:33 +0200

    Minor improvements.

--- frontend/src/app/services/dialog.service.ts
+++ frontend/src/app/services/dialog.service.ts
@@ -3,6 +3,8 @@ import {MatDialog, MatDialogRef} from '@angular/material';
 import {InfoDialogComponent} from '../dialogs/info-dialog.component';
 import {InputDialogComponent} from '../dialogs/input-dialog.component';
 
+const DIALOG_BOX_WIDTH: string = '450px';
+
 @Injectable()
 export class DialogService {
     constructor(public dialog: MatDialog) {}
@@ -11,7 +13,7 @@ export class DialogService {
         const buttonLabel: string = buttonText ? buttonText : 'Ok';
 
         const infoDialogRef = this.dialog.open(InfoDialogComponent, {
-            width: '450px',
+            width: DIALOG_BOX_WIDTH,
             data: {title: title, content: content, buttonText: buttonLabel}
         });
 
@@ -22,7 +24,7 @@ export class DialogService {
         const buttonLabel: string = buttonText ? buttonText : 'Submit';
 
         const inputDialogRef = this.dialog.open(InputDialogComponent, {
-            width: '450px',
+            width: DIALOG_BOX_WIDTH,
             data: {title: title, content: content, input: inputData, buttonText: buttonLabel}
         });
 
