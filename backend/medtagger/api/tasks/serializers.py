03aeb7f34637c0bb5655f83d304419dc8417de41
Piotr Walkusz <piotrwalkusz1@gmail.com>
2018-08-05 12:54:35 +0200

    Fix LabelTag serializers

--- backend/medtagger/api/tasks/serializers.py
+++ backend/medtagger/api/tasks/serializers.py
@@ -17,11 +17,9 @@ out__label_tag = api.model('Label Tag model', {
 in__label_tag = api.model('Label Tag model', {
     'key': fields.String(),
     'name': fields.String(),
-    'actions_ids': fields.List(fields.Integer(),
-                               attribute=lambda label_tag: [action.id for action in label_tag.actions]),
+    'actions_ids': fields.List(fields.Integer())
     'tools': fields.List(fields.String(), description='Available tools for Label Tag',
-                         enum=[tool.name for tool in LabelTool],
-                         attribute=lambda label_tag: [tool.name for tool in label_tag.tools]),
+                         enum=[tool.name for tool in LabelTool]),
 })
 
 out__task = api.model('Task model', {
