5c9f2f7e03d747a7d45a6707dbcc94b1303986ed
Piotr Walkusz <piotrwalkusz1@gmail.com>
2018-08-05 16:14:08 +0200

    ChainSelection is not loop by default

--- frontend/src/app/model/ChainSelection.ts
+++ frontend/src/app/model/ChainSelection.ts
@@ -3,42 +3,26 @@ import {Point} from './Point';
 
 export class ChainSelection extends SliceSelection {
 
-    private _points: Array<Point>;
-    private _isLoop: boolean;
+    points: Array<Point>;
+    isLoop = false;
 
-    constructor(points: Array<Point>, depth: number) {
+    constructor(points: Array<Point>, depth: number, tag: string) {
         super();
-        this._points = points;
+        this.points = points;
         this.sliceIndex = depth;
-        this.label_tag = 'LEFT_KIDNEY'; // TODO: Change these when introducing new marker page
+        this.label_tag = tag;
         this.label_tool = 'CHAIN';
     }
 
-    public get points() {
-        return this._points;
-    }
-
-    public set points(points: Array<Point>) {
-        this._points = points;
-    }
-
-    get isLoop(): boolean {
-        return this._isLoop;
-    }
-
-    set isLoop(value: boolean) {
-        this._isLoop = value;
-    }
-
     public getCoordinates() {
-        return {x: this._points[0].x, y: this._points[0].y, z: this.sliceIndex};
+        return {x: this.points[0].x, y: this.points[0].y, z: this.sliceIndex};
     }
 
     public toJSON() {
         return {
             'slice_index': this.sliceIndex,
-            'points': this._points,
-            'loop': this._isLoop,
+            'points': this.points,
+            'loop': this.isLoop,
             'tag': this.label_tag,
             'tool': this.label_tool
         };
