a45cb97fc55ec6cecf4217df3bd193502c0ef397
Jakub Powierza <jpowie01@gmail.com>
2018-08-12 19:38:46 +0200

    Allow to run script inside of Docker container

--- backend/scripts/dev__prepare_backend.sh
+++ backend/scripts/dev__prepare_backend.sh
@@ -9,7 +9,7 @@ echo "Apply database fixtures..."
 python3.6 medtagger/database/fixtures.py
 
 echo "Configuration synchronization..."
-cp -n .example.medtagger.yml .medtagger.yml
+cp -n .example.medtagger.yml .medtagger.yml || :
 python3.6 scripts/sync_configuration.py
 
 echo "Populate database with default user accounts..."
