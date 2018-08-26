1f756ef5273019c74993f342163df7818e9a66b4
kolszewska <k.olszewskaaa@gmail.com>
2018-08-26 21:29:57 +0200

    Fixed an issue with too long line

--- backend/medtagger/database/models.py
+++ backend/medtagger/database/models.py
@@ -177,7 +177,8 @@ class Scan(Base):
     owner_id: Optional[int] = Column(Integer, ForeignKey('Users.id'))
     owner: Optional[User] = relationship('User', back_populates='scans')
 
-    slices: List['Slice'] = relationship('Slice', back_populates='scan', cascade='delete', order_by=lambda: Slice.location)
+    slices: List['Slice'] = relationship('Slice', back_populates='scan', cascade='delete',
+                                        order_by=lambda: Slice.location)
     labels: List['Label'] = relationship('Label', back_populates='scan', cascade='delete')
 
     def __init__(self, dataset: Dataset, declared_number_of_slices: int, user: Optional[User]) -> None:
