4de735837e337b389fd97531e5838eae694a800b
kolszewska <k.olszewskaaa@gmail.com>
2018-08-26 21:24:04 +0200

    Fixed bug with old primary key name

--- backend/alembic/versions/9f2eafdf821e_add_cascade_delete_to_scans.py
+++ backend/alembic/versions/9f2eafdf821e_add_cascade_delete_to_scans.py
@@ -24,59 +24,69 @@ branch_labels = None
 depends_on = None
 
 
-def upgrade():
+def upgrade(): 
     op.drop_constraint('fk_Slices_scan_id_Scans', 'Slices')
+    op.create_foreign_key('fk_Slices_scan_id_Scans', 'Slices', 'Scans', ['scan_id'], ['id'], ondelete='CASCADE')
+
     op.drop_constraint('fk_Labels_scan_id_Scans', 'Labels')
-    
+    op.create_foreign_key('fk_Labels_scan_id_Scans', 'Labels', 'Scans', ['scan_id'], ['id'], ondelete='CASCADE')
+
     # Droping LabelSelections foreign key (instead of LabelElements) due to naming bug
     op.drop_constraint('fk_LabelSelections_label_id_Labels', 'LabelElements')
-
-    op.drop_constraint('fk_RectangularLabelElements_id_LabelElements', 'RectangularLabelElements')
-    # Droping PointLabelElement (without 's') due to naming bug
-    op.drop_constraint('fk_PointLabelElement_id_LabelElements', 'PointLabelElements')
-    op.drop_constraint('fk_ChainLabelElements_id_LabelElements', 'ChainLabelElements')
-    op.drop_constraint('fk_ChainLabelElementPoints_label_element_id_LabelElements', 'ChainLabelElementPoints')
-    op.drop_constraint('fk_BrushLabelElements_id_LabelElements', 'BrushLabelElements')
-
-    op.create_foreign_key('fk_Slices_scan_id_Scans', 'Slices', 'Scans', ['scan_id'], ['id'], ondelete='CASCADE')
-    op.create_foreign_key('fk_Labels_scan_id_Scans', 'Labels', 'Scans', ['scan_id'], ['id'], ondelete='CASCADE')
     op.create_foreign_key('fk_LabelElements_label_id_Labels', 'LabelElements', 'Labels', ['label_id'], ['id'], ondelete='CASCADE')
-    
+
+    # Droping PointLabelElement (without 's') due to naming bug
+    op.drop_constraint('fk_RectangularLabelElements_id_LabelElements', 'RectangularLabelElements')
     op.create_foreign_key(
         'fk_RectangularLabelElements_id_LabelElements', 'RectangularLabelElements', 'LabelElements', ['id'], ['id'], ondelete='CASCADE')
+
+    # Altering primary key name from LabelSelections to LabelElements
+    op.execute('ALTER INDEX "pk_LabelSelections" RENAME TO "pk_LabelElements"')
+
+    op.drop_constraint('fk_PointLabelElement_id_LabelElements', 'PointLabelElements')
     op.create_foreign_key(
         'fk_PointLabelElements_id_LabelElements', 'PointLabelElements', 'LabelElements', ['id'], ['id'], ondelete='CASCADE')
+
+    op.drop_constraint('fk_ChainLabelElements_id_LabelElements', 'ChainLabelElements')
     op.create_foreign_key(
         'fk_ChainLabelElements_id_LabelElements', 'ChainLabelElements', 'LabelElements', ['id'], ['id'], ondelete='CASCADE')
+
+    op.drop_constraint('fk_ChainLabelElementPoints_label_element_id_LabelElements', 'ChainLabelElementPoints')
     op.create_foreign_key(
         'fk_ChainLabelElementPoints_label_element_id_LabelElements', 'ChainLabelElementPoints', 'LabelElements', ['label_element_id'], ['id'], ondelete='CASCADE')
+    
+    op.drop_constraint('fk_BrushLabelElements_id_LabelElements', 'BrushLabelElements')
     op.create_foreign_key(
         'fk_BrushLabelElements_id_LabelElements', 'BrushLabelElements', 'LabelElements', ['id'], ['id'], ondelete='CASCADE')
-    # TODO: PK for LabelElements is LabelSelections
-    # TODO: PK for PointLabelElement is PointLabelElement
 
 def downgrade():
     op.drop_constraint('fk_Slices_scan_id_Scans', 'Slices')
-    op.drop_constraint('fk_Labels_scan_id_Scans', 'Labels')
-    op.drop_constraint('fk_LabelElements_label_id_Labels', 'LabelElements')
-    
-    op.drop_constraint('fk_RectangularLabelElements_id_LabelElements', 'RectangularLabelElements')
-    op.drop_constraint('fk_PointLabelElements_id_LabelElements', 'PointLabelElements')
-    op.drop_constraint('fk_ChainLabelElements_id_LabelElements', 'ChainLabelElements')
-    op.drop_constraint('fk_ChainLabelElementPoints_label_element_id_LabelElements', 'ChainLabelElementPoints')
-    op.drop_constraint('fk_BrushLabelElements_id_LabelElements', 'BrushLabelElements')
-
     op.create_foreign_key('fk_Slices_scan_id_Scans', 'Slices', 'Scans', ['scan_id'], ['id'])
+
+    op.drop_constraint('fk_Labels_scan_id_Scans', 'Labels')
     op.create_foreign_key('fk_Labels_scan_id_Scans', 'Labels', 'Scans', ['scan_id'], ['id'])
+
+    op.drop_constraint('fk_LabelElements_label_id_Labels', 'LabelElements')
     op.create_foreign_key('fk_LabelSelections_label_id_Labels', 'LabelElements', 'Labels', ['label_id'], ['id'])
-    
+
+    op.execute('ALTER INDEX "pk_LabelElements" RENAME TO "pk_LabelSelections"')
+
+    op.drop_constraint('fk_RectangularLabelElements_id_LabelElements', 'RectangularLabelElements')
     op.create_foreign_key(
         'fk_RectangularLabelElements_id_LabelElements', 'RectangularLabelElements', 'LabelElements', ['id'], ['id'])
+
+    op.drop_constraint('fk_PointLabelElements_id_LabelElements', 'PointLabelElements')
     op.create_foreign_key(
         'fk_PointLabelElement_id_LabelElements', 'PointLabelElements', 'LabelElements', ['id'], ['id'])
+
+    op.drop_constraint('fk_ChainLabelElements_id_LabelElements', 'ChainLabelElements')
     op.create_foreign_key(
         'fk_ChainLabelElements_id_LabelElements', 'ChainLabelElements', 'LabelElements', ['id'], ['id'])
+
+    op.drop_constraint('fk_ChainLabelElementPoints_label_element_id_LabelElements', 'ChainLabelElementPoints')
     op.create_foreign_key(
         'fk_ChainLabelElementPoints_label_element_id_LabelElements', 'ChainLabelElementPoints', 'LabelElements', ['label_element_id'], ['id'])
+
+    op.drop_constraint('fk_BrushLabelElements_id_LabelElements', 'BrushLabelElements')
     op.create_foreign_key(
         'fk_BrushLabelElements_id_LabelElements', 'BrushLabelElements', 'LabelElements', ['id'], ['id'])
